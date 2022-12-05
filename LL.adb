04edc5414644bd840a591877b4db4645103a247c
Absobel <imkonnu@gmail.com>
2022-12-05 20:59:49 +0100

    module liste chainée terminé

--- LL.adb
+++ LL.adb
@@ -1,6 +1,9 @@
 with Ada.Unchecked_Deallocation;
 
-package body LL is
+package body Liste_Chainee is
+
+    IndexOutofRange : exception;
+    DonneeManquante : exception;
 
 	procedure Free is
 		new Ada.Unchecked_Deallocation (T_Cellule, T_Pile);
@@ -24,7 +27,96 @@ package body LL is
     procedure Ajouter(Liste: in out T_Liste_Chainee; Donnee: in T_Donnee) is
         Nouveau: T_Cellule;
     begin
-        Nouveau := new T_Cellule'(Donnee, Liste);
+        while Liste /= null loop
+            Liste := Liste.all.Suivant;
+        end loop;
+        Nouveau := new T_Cellule'(Donnee, null);
+        Liste := Nouveau;
     end;
 
-end LL;
+    procedure AjouterApres(Liste: in out T_Liste_Chainee; Donnee: in T_Donnee; Index: in Integer) is
+        Nouveau: T_Cellule;
+        Aux: T_Liste_Chainee := Liste;
+    begin
+        if Index >= Taille(Liste) then
+            raise IndexOutofRange;
+        else
+            for Cpt in 1 .. Index loop
+                Aux := Aux.all.Suivant;
+            end loop;
+            Nouveau := new T_Cellule'(Donnee, Aux.all.Suivant);
+            Aux.all.Suivant := Nouveau;
+        end if;
+    end;
+
+    procedure Supprimer(Liste: in out T_Liste_Chainee; Index: in Integer) is
+        Aux: T_Liste_Chainee := Liste;
+        A_Supprimer: T_Liste_Chainee;
+    begin
+        if Index >= Taille(Liste) then
+            raise IndexOutofRange;
+        elsif Index = 0 then
+            Liste := Liste.all.Suivant;
+            Free(Aux);
+        else
+            for Cpt in 1 .. Index-1 loop
+                Aux := Aux.all.Suivant;
+            end loop;
+            A_Supprimer := Aux.all.Suivant;
+            Aux.all.Suivant := A_Supprimer.all.Suivant;
+            Free(A_Supprimer);
+        end if;
+    end;
+
+    function  Obtenir(Liste: T_Liste_Chainee; Index: Integer) return T_Donnee is
+        Aux: T_Liste_Chainee := Liste;
+    begin
+        if Index >= Taille(Liste) then
+            raise IndexOutofRange;
+        else
+            for Cpt in 1 .. Index loop
+                Aux := Aux.all.Suivant;
+            end loop;
+            return Aux.all.Donnee;
+        end if;
+    end;
+
+    function  IndexDe(Liste: T_Liste_Chainee; Donnee: T_Donnee) return Integer is
+        Aux: T_Liste_Chainee := Liste;
+        Cpt: Integer := 0;
+    begin
+        while Aux /= null or Aux.all.Donnee /= Donnee loop
+            Aux := Aux.all.Suivant;
+            Cpt := Cpt+1;
+        end loop;
+        if Aux.all.Suivant = null then
+            raise DonneeManquante;
+        else
+            return Cpt;
+        end if;
+    end;
+
+    function  EstDans(Liste: T_Liste_Chainee; Donnee: T_Donnee) return Boolean is
+        Aux: T_Liste_Chainee := Liste;
+    begin
+        while Aux /= null or Aux.all.Donnee /= Donnee loop
+            Aux := Aux.all.Suivant;
+        end loop;
+        if Aux.all.Suivant = null then
+            return false;
+        else
+            return true;
+        end if;
+    end;
+
+    procedure Detruire(Liste : in out T_Liste_Chainee) is
+        Aux: T_Liste_Chainee := Liste;
+    begin
+        while Liste /= null loop
+            Liste := Liste.all.Suivant;
+            Free(Aux);
+            Aux := Liste;
+        end loop;
+    end;
+
+end Liste_Chainee;
--- LL.ads
+++ LL.ads
@@ -1,6 +1,6 @@
 generic
     type T_Donnee is private;
-package LL is
+package Liste_Chainee is
 
     type T_Cellule;
     type T_Liste_Chainee is access T_Cellule;
@@ -9,10 +9,11 @@ package LL is
     procedure Initialiser(Liste: in out T_Liste_Chainee);
     function  Taille(Liste: T_Liste_Chainee) return Integer;
     procedure Ajouter(Liste: in out T_Liste_Chainee; Donnee: in T_Donnee);
-    procedure AjoterApres(Liste: in out T_Liste_Chainee; Donnee: in T_Donnee; Index: in Integer);
+    procedure AjouterApres(Liste: in out T_Liste_Chainee; Donnee: in T_Donnee; Index: in Integer);
     procedure Supprimer(Liste: in out T_Liste_Chainee; Index: in Integer);
     function  Obtenir(Liste: T_Liste_Chainee; Index: Integer) return T_Donnee;
     function  IndexDe(Liste: T_Liste_Chainee; Donnee: T_Donnee) return Integer;
+    function  EstDans(Liste: T_Liste_Chainee; Donnee: T_Donnee) return Boolean;
     procedure Detruire(Liste : in out T_Liste_Chainee);
 
 private
@@ -22,4 +23,4 @@ private
         Suivant : T_Liste_Chainee;
     end record;
 
-end LL;
+end Liste_Chainee;
